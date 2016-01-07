82ca4225a7bcc34d812d259b59805325f0422ee0
wohlfea <wohlfea@gmail.com>
2016-01-06 22:44:52 -0800

    scrolls to text upon clicking link

--- scripts/view.js
+++ scripts/view.js
@@ -7,25 +7,34 @@ projectView.handleTabs = function() {
     var target = e.target;
     console.log(target.id);
     if(target.id == 'navHome'){
+      menuView.close();
       $('.maindisp').hide();
       $('#projects').show();
+      projectView.scrollTo('#projects');
 
     } else if(target.id == 'navAbout'){
+      menuView.close();
       $('.maindisp').hide();
       $('#about').show();
+      projectView.scrollTo('#about');
     };
-
   });
-
+};
+projectView.scrollTo = function(x) {
+  $('body, html').animate({ scrollTop: $(x).offset().top - $('.mainnav ul').innerHeight() }, 500);
 };
 
 var menuView = {};
-
 menuView.hamburgerHandler = function () {
   $('.icon-menu').on('click', function(){
     $('nav ul').slideToggle('fast');
     $('.icon-menu').toggleClass('icon-cross');
   });
+};
+menuView.close = function() {
+  if ($('.menu-div i').hasClass('icon-cross') && $('.menu-div i').css('display') === 'inline') {
+    $('.icon-menu').trigger('click');
+  }
 };
 projectView.handleTabs();
 menuView.hamburgerHandler();
