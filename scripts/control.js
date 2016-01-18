ab7aac6ea4ceac2eb50aa489c215c1c27702c92b
wohlfea <wohlfea@gmail.com>
2016-01-18 15:41:24 -0800

    basic functionality working with page.js

--- scripts/control.js
+++ scripts/control.js
@@ -1,4 +1,28 @@
-projectView.handleTabs();
-menuView.hamburgerHandler();
-menuView.resizeListen();
-Project.checkStorage();
+var controller = {};
+
+controller.index = function() {
+  console.log('index selected');
+  menuView.close();
+  $('.main-disp').hide();
+  if($('#projects')[0]){
+    console.log('projects exists');
+    $('#projects').show();
+  } else {
+    console.log('checking storage');
+    Project.checkStorage();
+  }
+  projectView.scrollTo('#projects');
+};
+controller.about = function() {
+  menuView.close();
+  $('#projects').remove();
+  $('#about').show();
+  projectView.scrollTo('#about');
+};
+
+controller.init = function() {
+  menuView.hamburgerHandler();
+  menuView.resizeListen();
+};
+
+controller.init();
